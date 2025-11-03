519af779c37f51271666ffe522ecb7f06b885099
dianannperla <waafirmelyza@hotmail.com>
2025-11-03 10:35:21 +0000

    feat: initial commit for angular-startup-template/src/app/app.component.ts

---
+++ angular-startup-template/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Chào mừng bạn đến với thử nghiệm Angular!</h1>`,
+  styles: []
+})
+export class AppComponent {
+}
